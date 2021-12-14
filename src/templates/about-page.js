80d171074c31bef62ffbc8e6ecdd5f83f2eab617
hannarosenfeld <hannazrosenfeld@gmail.com>
2021-12-14 16:22:40 -0500

    Styling

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -91,16 +91,16 @@ const AboutPage = ({ data }) => {
 
 
 
-      
-		  <div style={{background: "#033277"}}>
-	  <div style={{width: "70%", display: "flex", margin: "0 auto", padding: "1em"}} className="newsletter-box">
-	  <h4 style={{color: "white", padding: "1em", fontSize: "2.5rem", fontWeight: "bold"}}>Keine Veranstaltung verpassen</h4>
+            
+	  <div style={{background: "#033277"}}>
+	  <div style={{width: "70%", display: "flex", margin: "0 auto", padding: "2em"}} className="newsletter-box">
+	  <h4 style={{color: "white", fontSize: "2.5rem", fontWeight: "bold"}}>Bleiben Sie informiert!</h4>
 	  <div style={{display: "flex", flexDirection: "column"}}>
-	  <p style={{color: "white", marginTop: "3em", fontSize: "1rem"}}>Wir informieren Sie unsere laufenden Projekte, wie ihre Spenden vor Ort Gutes tun und senden ihnen Einladungen zu unseren Veranstaltungen.</p>
+	  <p style={{color: "white", marginTop: "1em", fontSize: "1rem"}}>Wir informieren Sie unsere laufenden Projekte, wie ihre Spenden vor Ort Gutes tun und senden ihnen Einladungen zu unseren Veranstaltungen.</p>
 	  <Mailchimp />
 	  </div>
 	  </div>
-            </div>
+         </div>
 
       	  <div className="mitgliedschaft-testimony">
 <h4 style={{color: "white"}}>„Mit meiner Mitgliedschaft kann ich die großartige
