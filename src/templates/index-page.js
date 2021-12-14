b19b272126eb314ed3e54fba7751b8afcfea882a
hannarosenfeld <hannazrosenfeld@gmail.com>
2021-12-13 20:34:42 -0500

    Index

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -24,35 +24,40 @@ export const IndexPageTemplate = ({
   return (
 	  <div>
 	  <HomepageBanner style={{marginBottom: "5em"}}/>
-	  <div style={{margin: "3em"}}></div>
-          <div className="container" style={{display: "flex",alignItems: "center", flexDirection: "column"}}>
-	  <div style={{display: "flex", alignItems: "center", width: "100%"}} className="image-box">
-	  <img src={care}/>
-	  <img src={radicia} />
-	  </div>
-	  <div style={{margin: "4em auto"}}>
-          <h3 className="has-text-weight-semibold" style={{marginBottom: "1em"}}>
-	  Bärenherz-Neuigkeiten
-      </h3>
 	  
+	  <p style={{margin: "3em"}}></p>
+	  
+            <div className="container" style={{display: "flex",alignItems: "center", flexDirection: "column"}}>
+              <div style={{display: "flex", alignItems: "center", width: "77%"}} className="image-box">
+	        <img src={care}/>
+                <img src={radicia} />
+	      </div>
+	  
+	  <div style={{margin: "2em auto", width: "75%"}}>
+	  <div >
+ 	  <h3 className="has-text-weight-semibold">Headline</h3>
+	  <p style={{margin: "1em auto 4em auto"}}>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
+	  </div>
+	  </div>
+	  </div>
+      
+	  <div style={{background: "#033277", display: "flex", padding: "2em"}}>
+	  <h4 style={{color: "white", padding: "1em", fontSize: "2.5rem", fontWeight: "bold"}}>Bleiben Sie informiert!</h4>
 	  <div>
-          <BlogRoll />
-	  </div>
-          <div className="column is-12 has-text-centered">
-          <Link className="btn" to="/blog" style={{margin: "1em auto 3em auto"}}>
-          Read more
-          </Link>
-	  </div>
-	  
-          </div>
-          </div>
-	  	    <div style={{background: "#033277", display: "flex", padding: "2em"}}>
-	    <h4 style={{color: "white", padding: "1em", fontSize: "2.5rem", fontWeight: "bold"}}>Bleiben Sie informiert!</h4>
-	    <div>
-	    <p style={{color: "white", padding: "1em", fontSize: "1rem"}}>Wir informieren Sie unsere laufenden Projekte, wie ihre Spenden vor Ort Gutes tun und senden ihnen Einladungen zu unseren Veranstaltungen.</p>
+	  <p style={{color: "white", padding: "1em", fontSize: "1rem"}}>Wir informieren Sie unsere laufenden Projekte, wie ihre Spenden vor Ort Gutes tun und senden ihnen Einladungen zu unseren Veranstaltungen.</p>
+         </div>
+         </div>
 
-	    </div>
-	    </div>
+          <div className="container" style={{display: "flex",alignItems: "center", flexDirection: "column", marginTop: "5em"}}>
+	  <div>
+	  <h3 className="has-text-weight-semibold" style={{marginBottom: "1em", textAlign: "left"}}>Bärenherz-Neuigkeiten</h3>
+	        <div>
+                  <BlogRoll />
+	  </div>
+	  </div>
+	  </div>
+
+
 
 	  <div className="mitgliedschaft-testimony">
 <h4 style={{color: "white"}}>„Mit meiner Mitgliedschaft kann ich die großartige
