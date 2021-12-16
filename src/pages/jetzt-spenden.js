931eaf90661ec734fbe3771ba3ddf0ccda28acbf
hannarosenfeld <hannazrosenfeld@gmail.com>
2021-12-16 11:53:37 -0500

    Fix Spenden Module load

--- src/pages/jetzt-spenden.js
+++ src/pages/jetzt-spenden.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout";
-import RaiseNow from "../components/RaiseNow"
+import { Suspense, lazy } from 'react';
+
+const RaiseNow  = React.lazy(() => import("../components/RaiseNow"));
 
 const SpendenPage = () => (
 	<Layout>
 	<div>
+	<Suspense fallback={<div>Loading...</div>}>
 	<RaiseNow />
+	</Suspense>
 	</div>
 	</Layout>
 )
