0225b807e38cbbc0caa8be4c92d9bab0ab250752
USER <user@Lightings-Air.fios-router.home>
2021-12-18 17:49:16 -0500

    Make spendenform work

--- src/components/RaiseNow.js
+++ src/components/RaiseNow.js
@@ -1,18 +1,41 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import {Helmet} from "react-helmet"
-import Layout from "../components/Layout";
 
-
-const RaiseNow = () => (
-	<div>
+function Child({}) {
+        return(
+	<>
 	<Helmet>
 	<script type='text/javascript' aysnc>
 	{` window.rnw.tamaro.runWidget('.dds-widget-container', {language: 'de'}) `}
         </script>
-	<script  type='text/javascript' async src="https://tamaro.raisenow.com/brenh-1353/latest/widget.js"></script>
 	</Helmet>
-		    <div style={{width: "75%", margin: "4em auto"}} className="dds-widget-container"></div>
-	</div>
-)
+		<div style={{width: "75%", margin: "4em auto"}} className="dds-widget-container"></div>
+	  </>  
+
+
+    )
+}
+
+function RaiseNow() {
+    const [loaded, setLoaded] = useState([false])
+
+    useEffect(() => {
+	const scriptTag = document.createElement('script')
+	scriptTag.src='https://tamaro.raisenow.com/brenh-1353/latest/widget.js'
+	scriptTag.addEventListener('load', ()=> setLoaded(true))
+	document.body.appendChild(scriptTag)
+    }, []);
+
+    useEffect(() => {
+	if (!loaded) return;
+    }, [loaded])
+    
+    return (
+	<>
+	    {loaded ? <Child /> : 'not loaded'}
+	</>
+    )
+}
+
 
 export default RaiseNow
