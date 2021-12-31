c705848c1c614c752faeb6673cd1693ace0024a6
hannarosenfeld <hannazrosenfeld@gmail.com>
2021-12-31 16:42:15 -0500

    URGH

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-
+import impressum from "./impressum/index.json"
+import showdown from 'showdown'
 
 const Impressum = () => {
+    const converter = new showdown.Converter()
     return(
 	<Layout>
-	    <div style={{width: "80%", display: "flex", flexDirection: "column"}}>
-		<div style={{paddingLeft: "4em"}}>
+	    <div style={{width: "80%", display: "flex", flexDirection: "column", margin: "0 auto"}}>
+		<div style={{}}>
 <p>Bärenherz – Verein für humanitäre Hilfe</p>
 <p>c/o Rainer Mück</p>
 <p>Untersellenstrasse 27</p>
@@ -17,11 +19,10 @@ const Impressum = () => {
 	     <p><a href="mailto:info@baerenherz.org" >info@baerenherz.org</a></p> 
     </div>
 
-		<div style={{padding: "4em"}}>
-<h2>Disclaimer</h2>	    
-<p>Die in dieser Website enthaltenen Informationen sind allgemeiner Art. Sie sind weder dafür vorgesehen noch dazu geeignet, eine individuelle Beratung durch fachkundige Personen unter Berücksichtigung der konkreten Umstände des jeweiligen Einzelfalls zu ersetzen. Für weiterführende individuelle Informationen bitten wir Sie, die jeweils im Text genannten Personen und Stellen zu kontaktieren oder uns eine Online-Anfrage zu senden.</p>
-<br />
-<p>Aufgrund von Gesetzesrevisionen, sonstiger veränderter Umstände oder infolge technischer Fehler treten auf dieser Website möglicherweise Verspätungen, Lücken, Ungenauigkeiten oder sonstige Fehler auf. Bei der Zusammenstellung der Website haben wir uns um größtmögliche Sorgfalt bemüht. Dennoch machen wir keinerlei Zusagen über die Richtigkeit, fehlende Aktualität, Zuverlässigkeit und Vollständigkeit der auf dieser Website enthaltenen Informationen. In keinem Fall haftet Bärenherz oder mit ihr verbundene Gesellschaften oder deren Partner oder Angestellte für Verluste oder Schäden irgendwelcher Art aus der Verwendung dieser Informationen; (uneingeschränkt) eingeschlossen sind direkte, indirekte oder Folgeschäden, die aus dem Zugriff auf oder der Benutzung und Abfrage der Bärenherz Website oder aus der Verknüpfung (Linking) mit anderen Websites entstehen könnten, auch wenn Bärenherz auf die Möglichkeit solcher Schäden hingewiesen wurde. Verschiedene Verknüpfungen (Links) auf der Bärenherz Website verbinden Sie mit anderen Websites. Bärenherz hat die anderen, mit ihrer Website verknüpften Sites nicht geprüft und übernimmt für deren Inhalt keine Verantwortung. Der gesamte Inhalt der Bärenherz Website ist urheberrechtlich geschützt (alle Rechte vorbehalten). Das Herunterladen oder Ausdrucken einzelner Seiten oder Abschnitte der Bärenherz Website ist gestattet, sofern weder die Copyrightvermerke noch andere gesetzlich geschützte Bezeichnungen entfernt werden. Wenn Sie Software oder sonstige Daten von der Bärenherz Website herunterladen oder auf andere Weise vervielfältigen, bleiben sämtliche Eigentumsrechte bei Bärenherz. Das (vollständige oder teilweise) Reproduzieren, Übermitteln (elektronisch oder mit anderen Mitteln), Modifizieren, Verknüpfen oder Benutzen der Bärenherz Website für öffentliche oder kommerzielle Zwecke ist ohne vorherige schriftliche Zustimmung von Bärenherz nicht gestattet.</p>
+	    <div style={{margin: "2em auto"}}>
+	    <h2>Disclaimer</h2>
+	    <br/>
+	    	    <div dangerouslySetInnerHTML={{ __html: converter.makeHtml(impressum.body)}} />
 	    </div>
 		</div>
 	</Layout>
