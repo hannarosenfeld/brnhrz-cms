078b080115a11dfecb3d8db43c9a28830b15a8aa
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-02-07 12:01:13 -0500

    Cleanup.

--- src/components/RaiseNow.js
+++ src/components/RaiseNow.js
@@ -2,37 +2,37 @@ import React, { useState, useEffect } from "react"
 import {Helmet} from "react-helmet"
 
 function Child() {
-        return(
-	    <>
-	<Helmet>
-	<script type='text/javascript' aysnc>
-	{` window.rnw.tamaro.runWidget('.dds-widget-container', {language: 'de'}) `}
+  return(
+	<>
+	  <Helmet>
+	    <script type='text/javascript' aysnc>
+	      {` window.rnw.tamaro.runWidget('.dds-widget-container', {language: 'de'}) `}
         </script>
-	</Helmet>
-		    <div style={{width: "75%", margin: "4em auto"}} className="dds-widget-container"></div>
-	  </>  
-    )
+	  </Helmet>
+	  <div style={{width: "75%", margin: "4em auto"}} className="dds-widget-container"></div>
+	</>
+  )
 }
 
 function RaiseNow() {
-    const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false)
 
-    useEffect(() => {
+  useEffect(() => {
 	const scriptTag = document.createElement('script')
 	scriptTag.src='https://tamaro.raisenow.com/brenh-1353/latest/widget.js'
 	scriptTag.addEventListener('load', ()=> setLoaded(true))
 	document.body.appendChild(scriptTag)
-    }, []);
+  }, []);
 
-    useEffect(() => {
+  useEffect(() => {
 	if (!loaded) return;
-    }, [loaded])
-    
-    return (
+  }, [loaded])
+
+  return (
 	<>
-	    {loaded ? <Child /> : 'not loaded'}
+	  {loaded ? <Child /> : 'not loaded'}
 	</>
-    )
+  )
 }
 
 
