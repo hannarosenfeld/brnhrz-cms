d9de0456b216c1b3bfbbc1506acde7384d00f3a9
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-02-11 17:09:43 -0500

    Add to form.

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from "react"
 
+import "../styles/styles.css"
 
 function SignUpForm() {
   return(
@@ -36,23 +37,65 @@ function SignUpForm() {
           <label>Email<input type="email" name="email" /></label>
         </p>
       </div>
-      <div className="d-flex flex-column signupform-checkboxes">
+
+      <div className="d-flex flex-column signupform-checkboxes m-2">
         <p>
-          <label><input type="checkbox" name="teilnahmebestätigung"/>Ja, ich/wir nehmen am Charity Golf Cup 2022 teil.</label>
+          <input type="checkbox" name="teilnahmebestätigung"/><label>&nbsp;&nbsp;Ja, ich/wir nehmen am Charity Golf Cup 2022 teil.</label>
         </p>
         <p>
-          <label>
-            <input type="checkbox" name="gala"/>
-            Ich komme in Begleitung von
-            <input type="text" name="begleitung-name" />
-            <input type="text" name="begleitung-club" />
-          </label>
+          <label><input type="checkbox" name="nur-gala"/>&nbsp;&nbsp;Ich/ wir nehmen ausschliesslich an der Charity Golf Cup Gala teil.</label>
+        </p>
+        <p>
+          <input type="checkbox" name="gala"/>
+          <label>&nbsp;&nbsp;Ich komme in Begleitung von&nbsp;&nbsp;</label>
+          <input type="text" name="begleitung-name" />
+          <input type="text" name="begleitung-club" />
+        </p>
+        <p>
+          <input type="checkbox" class="signupform-checkbox" name="nur-gala"/>
+          <label>&nbsp;&nbsp;Ich/ wir nehmen als Privatpersonen teil.</label>
+          <p><label>Teilnahme:&nbsp;&nbsp;</label>
+            <input type="checkbox" name="privatpersonen-golf-und-gala"/>
+            <label>&nbsp;&nbsp;Golfturnier & Gala: 285,00 CHF&nbsp;&nbsp;&nbsp;&nbsp;</label>
+            <input type="checkbox" name="privatpersonen-nur-gala"/>
+            <label>&nbsp;&nbsp;Nur Gala: 285,00 CHF</label>
+          </p>
         </p>
       </div>
-      <br/>
-      <p>
-        <button type="submit">Send</button>
+
+
+      <div className="flug">
+        <input type="checkbox" name="nur-gala"/>
+        <label>&nbsp;&nbsp;Wir möchten einen eigenen Flight buchen</label>
+    <p>Kosten für einen 4er-Flight: 1.140,00 CHF</p>
+    <div class="flug-felder">
+      <p class="flug-row">
+        <div class="flugdetail"><input type="text" name="flugticket-1-vorname" /><label>Vorname</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-1-name" /><label>Name</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-1-heimatclub-hcp" /><label>Heimatclub, Hcp.</label></div>
       </p>
+      <p class="flug-row">
+        <div class="flugdetail"><input type="text" name="flugticket-2-vorname" /><label>Vorname</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-2-name" /><label>Name</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-2-heimatclub-hcp" /><label>Heimatclub, Hcp.</label></div>
+      </p>
+      <p class="flug-row">
+        <div class="flugdetail"><input type="text" name="flugticket-3-vorname" /><label>Vorname</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-3-name" /><label>Name</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-3-heimatclub-hcp" /><label>Heimatclub, Hcp.</label></div>
+      </p>
+      <p class="flug-row">
+        <div class="flugdetail"><input type="text" name="flugticket-4-vorname" /><label>Vorname</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-4-name" /><label>Name</label></div>
+        <div class="flugdetail"><input type="text" name="flugticket-4-heimatclub-hcp" /><label>Heimatclub, Hcp.</label></div>
+      </p>
+        </div>
+    </div>
+
+    <br/>
+    <p>
+      <button type="submit">Send</button>
+    </p>
     </form>
   )
 }
