4e2d5281ad517b7c2b87bb42c90fb49e83440019
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-02-12 13:32:59 -0500

    Test.

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -14,23 +14,65 @@ function SignUpForm() {
         <header className="signup-banner"><img src={golferin} /></header>
         <h2 style={{background: "#00aeef", color: "white", textAlign: "center", letterSpacing: "0.2em", fontSize: "1.8rem"}}>ANMELDUNG</h2>
       </div>
-      <Form className="signupform" name="turnier-form" method="POST" data-netlify="true" style={{margin: "0 auto", width: "70%"}}>
+      <Form className="signupform" name="turnier-form" method="POST" data-netlify="true" >
         <input type="hidden" name="form-name" value="turnier-form" />
-        <p style={{margin: "1em auto"}}>Bitte senden Sie uns Ihre Anmeldung per Email: <b>info@baerenherz.org</b></p>
-        <Form.Group className="mb-3" controlId="formBasicVorName">
-          <Form.Control type="email" placeholder="Vorname" name="vorname" />
-          <Form.Label>Vorname</Form.Label>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Control type="text" placeholder="Name" name="name" />
-          <Form.Label>Name</Form.Label>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" name="checkbox" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <div style={{margin: "0 auto", width: "90%"}}>
+          <p style={{margin: "1em auto"}}>Bitte senden Sie uns Ihre Anmeldung per Email: <b>info@baerenherz.org</b></p>
+
+          <div className="d-flex flex-wrap form-address-section">
+
+            <Form.Group className="form-field" controlId="formBasicVorName">
+              <Form.Control type="text" name="vorname" />
+              <Form.Label>Vorname</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="text" name="name" />
+              <Form.Label>Name</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="text" name="firma" />
+              <Form.Label>Firma, Funktion</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="text" name="heimatclub"/>
+              <Form.Label>Heimatclub</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="text" name="strasse" />
+              <Form.Label>Strasse, Nr.</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="text" name="plz" />
+              <Form.Label>PLZ, Ort</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="text" name="telefon" />
+              <Form.Label>Telefon/Telefax</Form.Label>
+            </Form.Group>
+
+            <Form.Group className="form-field" controlId="formBasicName">
+              <Form.Control type="email" name="email" />
+              <Form.Label>Email</Form.Label>
+            </Form.Group>
+
+          </div>
+          <div>
+            <Form.Group className="form-field" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" name="checkbox" />
+          </Form.Group>
+          </div>
+
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
       </Form>
     </main>
   )
