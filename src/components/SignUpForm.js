f6c8c9b968d67da9cf0a90f8e496f0e8b7da3972
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-02-25 10:53:47 -0500

    Test.

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -26,15 +26,11 @@ function SignUpForm() {
         <input type="hidden" name="form-name" value="turnier-form" />
         <div style={{margin: "0 auto", width: "90%"}}>
           <p style={{margin: "1em auto"}}>Bitte senden Sie uns Ihre Anmeldung per Email: <b>info@baerenherz.org</b></p>
+          <Form.Group classNamme="" controlId="formBasicName">
+            <Form.Control type="text" name="name" />
+            <Form.Label>Vorname, Name</Form.Label>
+          </Form.Group>
           <div className="d-flex flex-wrap form-address-section mb-4">
-            <Form.Group className="form-field" controlId="formBasicVorName">
-              <Form.Control type="text" name="vorname" />
-              <Form.Label>Vorname</Form.Label>
-            </Form.Group>
-            <Form.Group className="form-field" controlId="formBasicName">
-              <Form.Control type="text" name="name" />
-              <Form.Label>Name</Form.Label>
-            </Form.Group>
             <Form.Group className="form-field" controlId="formBasicFirma">
               <Form.Control type="text" name="firma" />
               <Form.Label>Firma, Funktion</Form.Label>
