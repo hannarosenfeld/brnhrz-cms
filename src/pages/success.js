58ebf44f71b593548f88e81679bd589a2e4f8997
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-02-25 12:55:44 -0500

    Styling success page.

--- src/pages/success.js
+++ src/pages/success.js
@@ -8,21 +8,22 @@ const SuccessPage = () => {
   return(
     <main className="success-page">
       <div className="success-modal-container">
-        <p>Herzlichen Dank für Ihre Anmeldung zum Charity Golf Turnier am 6. Mai 2022 in Lipperswil.</p>
-        <p>Bitte beachten Sie, dass Sie die finale Anmeldebestätigung erst dann erhalten, wenn Sie die Teilnahmegebühr auf unser Konto bei der Thurgauer Kantonalbank überwiesen haben (IBAN Nr.  CH91 0078 4297 6098 0200 1).</p>
-        <p>Wir freuen uns auf Sie und grüssen Sie ganz herzlich.</p>
-        <br/>
-        <div>
-          <p>Der Vorstand</p>
-          <p>Bärenherz - Verein für humanitäre Hilfe</p>
-          <p><a href="www.baerenherz.org">www.baerenherz.org</a></p>
-        </div>
-        <section className="mt-5">
-          <a href="/">
-            <ArrowLeft />
-            <span className="m-1"></span>Zurück zu Bärenherz
-          </a>
-        </section>
+        <section>Herzlichen Dank für Ihre Anmeldung zum Charity Golf Turnier am 6. Mai 2022 in Lipperswil.</section>
+          <section className="mt-2"> Bitte beachten Sie, dass Sie die finale Anmeldebestätigung erst dann erhalten, wenn Sie die Teilnahmegebühr auf unser Konto bei der Thurgauer Kantonalbank überwiesen haben (IBAN Nr.  CH91 0078 4297 6098 0200 1).</section>
+
+          <section className="mt-2">Wir freuen uns auf Sie und grüssen Sie ganz herzlich.</section>
+
+          <div className="mt-4">
+            <p>Der Vorstand</p>
+            <p>Bärenherz - Verein für humanitäre Hilfe</p>
+            <p><a href="www.baerenherz.org">www.baerenherz.org</a></p>
+          </div>
+          <section className="mt-5">
+            <a href="/">
+              <ArrowLeft />
+              <span className="m-1"></span>Zurück zu Bärenherz
+            </a>
+          </section>
       </div>
     </main>
   )
